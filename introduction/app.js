f06942fa49c57796b16583b86cf0500ae92e3f82
Vamsee1317 <vamsee.achieversit@gmail.com>
2025-03-03 21:07:41 +0530

    Js Intro | Implementation

---
+++ introduction/app.js
@@ -0,0 +1,11 @@
+const btn = document.getElementById("btn");
+
+
+console.log(btn);
+
+    btn.addEventListener('click', ()=>{
+        alert('You have Clicked Me from External');
+    })
+
+
+console.log("Hello World!!!");
---
+++ introduction/theory.js
@@ -0,0 +1,27 @@
+// What is Javascript
+
+    // Js is a scripting langauge used to make web pages responsive.
+    // It runs in the browser(client-side script), but can also be used in the server side(NodeJs).
+
+    // Example uses of Js :
+        // Form Validations
+        // Dynamic UI changes
+        // Interactive Buttons & Sliders
+        // Fetching Data from API's
+
+// Why Javascript
+    // Js is the most widely used language in the world.
+    // It is used by 95% of websites for client-side scripting.
+    // It is also used in mobile, desktop, Games...etc applications.
+    // It is a versatile language that can be used for both front-end and back-end development.
+    // It is also used in Machine Learning and Artificial Intelligence.
+    // It is a language that is easy to learn and has a lot of resources available.
+
+// ways to add js in HTML
+    // Inline Js 
+    // Internal JS (not recommended for Large scale projects)
+    // External Js
+
+
+// How to run javascript (Browser & Console & Terminal)
+            
