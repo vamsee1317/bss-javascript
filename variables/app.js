52d4d7f8f5042ab1b1342a6c35260d4e268c2f69
vamsee1317 <vamsee.achieversit@gmail.com>
2025-03-04 21:07:42 +0530

    Variables

---
+++ variables/app.js
@@ -0,0 +1,79 @@
+
+
+// Variables :
+    // Variable is a container helps us to store the data or information.
+    // Every container will have a name give which is known as identifier.
+
+
+// Js Variables :
+
+    // Declaration : creating a container
+    // Assigning / Initialization : adding a value
+    // Reassigning / ReInit : modfiy the value
+    // ReDeclaration : creating a duplicate container
+
+
+// Java(complier) vs Javascript(Interpreter)
+
+// ES5 version
+
+    // var
+
+// ES6 version (2015)
+
+    // let
+    // const
+
+
+// EmployeeInformation :
+
+// const employeeName = "VamseeKrishna";
+// console.log(employeeName);
+
+// employeeName = "VamseeKrishna";
+
+// employeeName = "VamsiKrishna";
+// console.log(employeeName);
+
+// const employeeName = "HariKrishna";
+// console.log(employeeName);
+
+
+
+
+// var vs let vs const
+
+//      Declaration         Assigning       Reassigning         Redeclaration
+// var        Yes                Yes               Yes                 Yes(****)
+// let        Yes                Yes               Yes                  No
+// const      Yes                Yes               No                   No
+
+
+
+
+// Javascript is loosely coupled langauge
+// Js is dynamically typed langauge
+
+
+// collecting userInformation in c langauge:
+
+// char name = "Vamsee";
+// int age = 25;
+// float salary = 50000.229;
+// char gender = 'M';
+// char maritalStatus = 'UM';
+// char designation = 'SE';
+
+
+// collecting employeeeInformation in Js langauge:
+
+const employeeName = "VamseeKrishna";
+let employeeAge = 25;
+let employeeSalary = 50000.229;
+const employeeGender = 'M';
+let employeeMaritalStatus = 'UM';
+let employeeDesignation = 'Sr.SE';
+
+// Note : const variable should be declared & assigned in a same line.
+
+
